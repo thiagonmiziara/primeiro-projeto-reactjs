769da5456ad1c44c43439c8ebae09c942d9ac7ea
thiagonmiziara <thiagonmiziara@gmail.com>
2020-12-05 14:51:22 -0300

    finalizando mais uma aula

--- ceep/src/conponents/ListaDeCategorias/ListaDeCategorias.jsx
+++ ceep/src/conponents/ListaDeCategorias/ListaDeCategorias.jsx
@@ -1,9 +1,14 @@
-import React, {Component} from 'react';
-import './estilo.css';
-class ListaDeCategorias extends Component{
-  
-  render(){
-    return(
+import React, { Component } from "react";
+import "./estilo.css";
+class ListaDeCategorias extends Component {
+
+  _handleEventoInput(event){
+    if(event.key === "Enter"){
+      
+    }
+  }
+  render() {
+    return (
       <section className="lista-categorias">
         <ul className="lista-categorias_lista">
           <li className="lista-categorias_item">Categorias</li>
@@ -11,7 +16,12 @@ class ListaDeCategorias extends Component{
           <li className="lista-categorias_item">Categorias</li>
           <li className="lista-categorias_item">Categorias</li>
         </ul>
-        <input type="text" className="lista-categorias_input"/>
+        <input
+          type="text"
+          className="lista-categorias_input"
+          placeholder="Adicionar categoria"
+          onKeyUp={this._handleEventoInput.bind(this)}
+        />
       </section>
     );
   }
